952b0199254305f32d3b18d5d058a4b5cfb0d3d7
kozhamuratoval <lianakozamuratova@gmail.com>
2026-06-07 01:52:53 +0500

    new

--- backend/app/modules/auth/presentation/schemas/requests/login_request.py
+++ backend/app/modules/auth/presentation/schemas/requests/login_request.py
@@ -3,4 +3,4 @@ from pydantic import BaseModel, Field
 
 class LoginRequest(BaseModel):
     email: str
-    password: str = Field(min_length=8, max_length=128)
+    password: str = Field(min_length=4, max_length=128)
